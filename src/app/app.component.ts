e3f37df4403088a3bd44f09e261bec24dec354a5
Armstrong <Nelh@warpdevelopment.com>
2022-01-12 15:56:35 +0200

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent  implements OnInit {
   title = 'otp-app';
+
+  otp!: string;
+  inputDigitLeft: string = "Verify code";
+  btnStatus: string = "btn-light";
+
+  public configOptions = {
+    length: 6,
+    inputClass: 'digit-otp',
+    containerClass: 'd-flex justify-content-between'
+  }
+  
+  ngOnInit() {
+      
+  }
+
+  onOtpChange(event: any) {
+    this.otp = event;
+    if(this.otp.length < this.configOptions.length) {
+      this.inputDigitLeft = this.configOptions.length - this.otp.length + " digits Left";
+      this.btnStatus = 'btn-light';
+    }
+
+    if(this.otp.length == this.configOptions.length) {
+      this.inputDigitLeft = "Let's go!";
+      this.btnStatus = 'btn-primary';
+    }
+  }
 }
